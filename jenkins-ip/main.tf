6ce5d95818da86e115150c57b643cc8bba4b803a
India <India@India-PC>
2023-10-15 01:40:53 +0530

    m

---
+++ jenkins-ip/main.tf
@@ -0,0 +1,20 @@
+	terraform {
+	  backend "s3" {
+	    bucket = "terraform-sd18"
+	    key    = "misc/jenkins-ip/terraform.tfstate"
+	    region = "us-east-1"
+	
+	  }
+	}
+	
+	data "aws_instance" "instance" {
+	  instance_id = "i-08e5d92f25193ed68"
+	}
+	
+	resource "aws_route53_record" "jenkins" {
+	  name    = "jenkins.sddevops18.online"
+	  type    = "A"
+	  zone_id = "Z0531070279OA6E0HE9DV"
+	  records = [data.aws_instance.instance.public_ip]
+	  ttl = 10
+	}
